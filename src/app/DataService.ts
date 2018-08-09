db51d8dc81adfb9fd8e4817d36b9f9da44f2788f
HarithaBandari <harithabandari154@gmail.com>
2018-08-09 11:30:11 +0530

    initial commit

--- src/app/DataService.ts
+++
@@ -1,9 +0,0 @@
-export class DataService { 
-    private data = [];
-    getRandomData(){
-    return this.data;
-    }
-    insertData(data:string){
-        this.data.push(data);
-    }
-}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'module1';
-  private data=""
-  // getData(data){
-  //  
-  // }
-  eventHandler(value:string) {
-    //Some code
-   this.data+=value;
-   console.log(this.data);
-}}
+  title = 'module3';
+ 
+}
